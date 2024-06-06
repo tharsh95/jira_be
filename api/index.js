a5b1f832c884ff6a3f138d7aae759ced98212318
Harsh Tiwari <harshtiwari@Harshs-MacBook-Air.local>
2024-06-06 12:18:32 +0530

    ic

---
+++ api/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+
+app.get("/jira", async (req, res) => {
+  try {
+
+    res.status(200).json({ data:"HW" });
+  } catch (e) {
+    console.log(e, "e");
+  }
+});
+
+app.listen(5555, () => console.log("Running"));
+
+
+export default app
